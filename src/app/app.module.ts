1a2ccb5f70309b28f3b916d6f942ffe7e1df6112
monorailbg <immanuel.hoehne@gmail.com>
2023-03-22 15:52:26 +0100

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,30 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './components/app-component/app.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+import { ServicesComponent } from './components/services/services.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { LoginComponent } from './components/login/login.component';
+import { DesignComponent } from './components/design/design.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    ServicesComponent,
+    NavbarComponent,
+    FooterComponent,
+    LoginComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    DesignComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { LoginComponent } from '../login/login.component';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent {
+  constructor(public dialog: MatDialog) {
+
+  }
+
+
+
+  login(): void{
+    const dialog = this.dialog.open(LoginComponent)
+  }
+}
